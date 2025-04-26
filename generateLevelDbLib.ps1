a3199cd4a33e7834861c937c36aa575fd67037db
SuperLlama88888 <116475184+SuperLlama88888@users.noreply.github.com>
2025-04-26 19:48:16 +1000

    v1.0.2
    - Make pako an external module
    - Add more info to README.md

--- generateLevelDbLib.ps1
+++ generateLevelDbLib.ps1
@@ -6,4 +6,4 @@ git clone https://github.com/Mojang/minecraft-creator-tools.git
 cd minecraft-creator-tools/app
 npm i
 npm i -g esbuild
-esbuild src/minecraft/LevelDb.ts --bundle --minify --platform=browser --format=esm --outfile=../../LevelDb.js --banner:js="// Copyright (c) Microsoft Corporation. Licensed under the MIT License."
+esbuild src/minecraft/LevelDb.ts --bundle --external:pako --minify --platform=browser --format=esm --outfile=../../LevelDb.js --banner:js="// Copyright (c) Microsoft Corporation. Licensed under the MIT License."
